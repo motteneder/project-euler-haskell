94254b96dcb78f9c696cd5fd79fcdb90333405ee
Michael Otteneder <m.otteneder@gmail.com>
2008-09-22 18:44:03 +0200

    refactore euler12.hs, now uses a list instead of a triple

--- euler4.hs
+++ euler4.hs
@@ -3,7 +3,6 @@ isPalindrome n = reverse (show n) == (show n)
 
 prop_isPalindrome n = reverse (show n) == show n
   where types = n::Int
-
-solve = let nums = [x * y| x <-[100..999] , y <- [100..999]]
+asdy <- [100..999]]
         in 
           show(last(takeWhile (isPalindrome) nums))
--- euler9.hs
+++ euler9.hs
@@ -1,19 +1,9 @@
 import System.Random
-
-max3 , min3, middle :: Int -> Int -> Int -> Int
-
-max3  a b c = max (max a b) c
-
-min3  a b c = min (min a b) c
-
-middle a b c = max (min a b) (min b c)
-
-sort3 :: Int -> Int -> Int -> (Int,Int,Int)
-
-sort3 a b c = ((min3 a b c), (middle a b c), (max3 a b c))
+import Data.List
 
 
-genTriple :: IO (Int, Int, Int)
+
+genTriple :: IO [Int]
 genTriple = do                 
   r1 <- randomRIO (0,1000)
   let a = r1
@@ -21,14 +11,18 @@ genTriple = do
   let b = r2
   let c = 1000 - (a + b)
 
-  return (a, b, c)
+  return [a, b, c]
 
-isPythagorean :: (Int, Int, Int) -> Bool
-isPythagorean (a, b, c) 
+isPythagorean :: [Int] -> Bool
+isPythagorean (x:xs) 
     |a < b && b < c = (a*a) + (b*b) == (c*c)
     | otherwise = False
+    where
+      a = x
+      b = head xs
+      c = last xs
 
-findPythagoreanTriple :: IO (Int, Int, Int)
+findPythagoreanTriple :: IO [Int]
 findPythagoreanTriple = do
   triple <- genTriple
   if isPythagorean triple then return triple else  findPythagoreanTriple
